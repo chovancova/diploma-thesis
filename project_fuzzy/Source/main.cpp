f9662903b9c252a3429a6192eb2c43c10e8a3043
Oľga Chovancová <chovancova.olga@yahoo.com>
2016-10-15 16:10:13 +0200

    Refactoring

---
+++ project_fuzzy/Source/main.cpp
@@ -0,0 +1,45 @@
+#include "TempFunctions.h"
+#include "DataSets/DataSets.h"
+#include "Fuzzy/fuzzy.h"
+
+void run_data_sets(unsigned int numDB)
+{
+	float        error;
+	DataSets initialData(numDB);
+	initialData.ReadDataSets(numDB);
+	initialData.WriteCrispFile();     // ********* Check of initial data
+
+	initialData.InitialError(numDB); 
+
+	error = initialData.InitialError(numDB);
+
+	printf("Press any key... TotalSets = %d   InitialError =%f", initialData.ReturnTotalSets(), error);
+
+	getchar();
+	getchar();
+}
+
+
+void run_fuzzy(unsigned int numDB)
+{
+	Fuzzy newA(numDB); 
+	newA.ReadDataSets(numDB);
+	newA.WriteCrispFile();
+	newA.StartFuzzification(numDB);
+	printf("Fuzzyfication is finished.  Press any key...");
+}
+
+int main()
+{
+	unsigned int numDB = _IRIS;
+	run_data_sets(numDB);
+	run_fuzzy(numDB);
+	
+	unsigned int numDB2 = _HEART;
+	run_data_sets(numDB2);
+	run_fuzzy(numDB2);
+
+	getchar();
+
+	return 0;
+}
